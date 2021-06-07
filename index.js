485b73a4976ea10e851b5ecb444ef772af7dfa41
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-07 23:41:23 +0900

    ADD JS-CH12

--- index.js
+++ index.js
@@ -1,83 +1,31 @@
 
-//예제 11-01
+//예제 12-01
+//f(x, y) = x + y
+function add(x, y) {
+    return x + y;
+}
 
-//  const 키워드를 사용해 선언한 변수는 재할당이 금지된다. 상수는 재할당이 금지된 변수일 뿐이다.
-const o = {};
-//const 키워드를 사용해 선언한 변수에 할당한 원시 값은 변경할 수 없다
-//하지만 const키워드를 사용해 선언한 변수에 할당한 객체는 변경할 수 있다.
-o.a = 1;
-console.log(o);
-
-//예제 11-04
-var str = 'string';
-
-console.log(str[0]);
-
-console.log(str.length);
-console.log(str.toUpperCase());
+add(2, 5); // 7
 
 
-//예제 11-05
-var str = 'string';
+//예제 12-02
+//함수 정의
+function add(x, y) {
+    return x + y;
+}
 
-str[0] = 'S';
-console.log(str);
+//예제 12-03
+//함수 호출
+var result = add(2, 5);
 
-//예제 11-08
-var score = 80;
+//함수 add에 인수 2, 5를 전달하면서 호출하면 반환값 7을 반환한다
+console.log(result);
 
-var copy = score;
-console.log(score, copy); // 80 80
-console.log(score === copy); // true
+//예제 12-04
+//변수에 함수 리터럴을 할당
+var f = function add(x, y) {
+    return x + y;
+}
 
-score = 100;
-console.log(score, copy); // 100 80
-console.log(score === copy); // fales
-
-//예제 11-13
-var person = {
-    name: 'Lee'
-};
-
-//프로퍼티 값 갱신
-person.name = 'Kim';
-
-//프로퍼티 동적 생성
-person.address = 'Seoul';
-
-console.log(person); // { name: 'Kim', address: 'Seoul' }
-
-
-//예제 11-17
-var person = {
-    name: 'Lee'
-};
-//참조 값을 복사. 동일한 참조 값을 갖는다
-var copy = person;
-
-console.log(copy === person); //true
-
-//copy흫 통해 객체를 변경한다.
-copy.name = 'Kim';
-
-//person을 통해 객체를 변경한다.
-person.address = 'Seoul';
-
-//copy와 person은 동일한 객체를 가리킨다.
-//따라서 어느 한쪽에서 객체를 변경하면 서로 영향을 주고 받는다.
-
-console.log(copy); // { name: 'Kim', address: 'Seoul' }
-console.log(person); // { name: 'Kim', address: 'Seoul' }
-
-//예제 11-18
-
-var person1 = {
-    name: 'Lee'
-};
-
-var person2 = {
-    name: 'Lee'
-};
-
-console.log(person1 === person2); // false
-console.log(person1.name === person2.name); //true
+//예제 12-01
+//예제 12-01

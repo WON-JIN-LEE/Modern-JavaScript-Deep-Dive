9419646738e1333c8a09478ac18a63095a02afa3
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-08 19:14:40 +0900

    Update JS-CH.12

--- index.js
+++ index.js
@@ -1,31 +1,79 @@
 
-//예제 12-01
-//f(x, y) = x + y
+
+//예제 12-05
+//함수 선언문
 function add(x, y) {
     return x + y;
 }
+//함수 참조
+console.log(add);// f add(x,y)
 
-add(2, 5); // 7
-
-
-//예제 12-02
-//함수 정의
-function add(x, y) {
-    return x + y;
-}
-
-//예제 12-03
 //함수 호출
-var result = add(2, 5);
+console.log(add(2, 5));
 
-//함수 add에 인수 2, 5를 전달하면서 호출하면 반환값 7을 반환한다
-console.log(result);
 
-//예제 12-04
-//변수에 함수 리터럴을 할당
-var f = function add(x, y) {
+//예제 12-06
+//함수 선언문은 함수 이름을 생략할 수 없다.
+function add(x, y) {
+    return x + y;
+}
+// SymtaxEror : Function statements require a function name
+
+//예제 12-09
+var add = function add(x, y) {
+    return x + y;
+};
+
+console.log(add(2, 5)); // 7
+
+
+//예제 12-10
+var add = function (x, y) {
+    return x + y;
+};
+
+console.log(add(2, 5)); // 7
+
+
+
+//예제 12-11
+var add = function foo(x, y) {
+    return x + y;
+};
+
+//함수 객체를 가리키는 식별자로 호출
+console.log(add(2, 5)); // 7
+
+//함수 이름으로 호출하면
+console.log(foo(2, 5)); // ReferenceError
+
+//예제 12-12
+//함수 참조
+console.dir(add);
+console.dir(sub);
+
+//함수 호출
+console.log(add(2, 5));
+console.log(sub(2, 5));
+
+//함수 선언문
+function add(x, y) {
     return x + y;
 }
 
+//함수 표현식
+var sub = function (x, y) {
+    return x - y;
+};
+
+//예제 12-13
+var add = new Function('x', 'y', 'return x + y');
+console.log(add(2, 5)); // 7
+
+//예제 12-15
+const add = (x, y) => x + y;
+console.log(add(2, 5)); // 7
+//예제 12-01
+//예제 12-01
 //예제 12-01
 //예제 12-01

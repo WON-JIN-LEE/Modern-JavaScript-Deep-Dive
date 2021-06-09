da2f0ebbd1f352c07327f258939d79a03c4ba77e
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-09 22:00:27 +0900

    Update JS-CH.12

--- index.js
+++ index.js
@@ -188,9 +188,146 @@ function multiply(x, y) {
 
 console.log(multiply(3, 5)); //undefined
 
-//예제 12-01
-//예제 12-01
-//예제 12-01
+//예제 12-33
+function changeVal(primitive, obj) {
+    primitive += 100;
+    obj.name = 'Kim';
+}
+
+var num = 100;
+var person = { name: 'Lee' };
+
+console.log(num); // 100
+console.log(person); //  { name: "Lee" }
+
+changeVal(num, person);
+
+console.log(num); // 100
+console.log(person); //  { name: "Lee" }
+
+//예제 12-34
+// 익명 즉시 실행함수
+(function () {
+    var a = 3;
+    var b = 5;
+    return a * b;
+}());
+
+//예제 12-42
+//즉시 실행함수도 일반 함수처럼 값을 반환할 수 있다.
+var res = (function () {
+    var a = 3;
+    var b = 5;
+    return a * b;
+}());
+
+console.log(res); //15
+
+//즉시 실행 함수에도 일반 함수처럼 인수를 전달할 수 있다.
+res = (function (a, b) {
+    return a * b;
+}(3,5));
+
+console.log(res); //15
+
+//예제 12-44
+function countdown(n) {
+    if (n < 0) return;
+    console.log(n);
+    countdown(n - 1);//재귀 호출
+}
+
+countdown(10);
+
+//예제 12-46
+//함수 표현식
+var factorial = function foo(n) {
+    //탈출 조건 : n이 1 이하일 떄 재귀 호출을 멈춘다.
+    if (n <= 1) return 1;
+    //함수를 가리키는 식별자로 자기 자신을 재귀 호출
+    return n * factorial(n - 1);
+
+
+    //함수 이름으로 자기 자신을 재귀 호출할 수도 있다.
+    // console.log(factorial === foo); //true
+    // return n * foo(n - 1);
+
+
+};
+
+console.log(factorial(5)); // 5! = 5*4*3*2*1 = 120
+
+//예제 12-48
+function outer() {
+    var x = 1;
+
+    //중첩 함수
+    function inner() {
+        var y = 2;
+        console.log(x + y); //3
+    }
+
+    inner();
+}
+
+
+outer();
+
+//예제 12-51
+//외부에서 전달받은 f를 n만큼 반복 호출한다.
+function repeat(n, f) {
+    for (var i = 0; i < n; i++){
+        f(i);
+    }
+}
+
+var logAll = function (i) {
+    console.log(i);
+};
+
+//반복 호출할 함수를 인수로 전달한다.
+repeat(5, logAll); //0 1 2 3 4
+
+var logOdds = function (i) {
+    if (i % 2) console.log(i);
+};
+
+repeat(5, logOdds); // 1 3
+
+//예제 12-53
+// logOdds 함수는 단 한 번만 생성된다.
+var logOdds = function (i) {
+    if (i % 2) console.log(i);
+};
+
+repeat(5, logOdds); // 1 3
+
+//예제 12-56
+var count = 0;
+
+function increase(n) {
+    return ++n;
+}
+
+count = increase(count);
+console.log(count); //1
+
+count = increase(count);
+console.log(count); //2
+
+//예제 12-57
+var count = 0;
+
+function increase(n) {
+    return ++count; //외부 상태의 의존하며 외부 상태를 변경한다.
+}
+
+increase();
+console.log(count); //1
+
+increase();
+console.log(count); //2
+
 //예제 12-01
 //예제 12-01
 //예제 12-01

bd8c469cb15febd458e15d8ac858b95ca6bcb175
WON-JIN-LEE <vopgh0409@gmail.com>
2021-06-14 22:03:39 +0900

    Update JS-CH16, 17

--- index.js
+++ index.js
@@ -1,330 +1,309 @@
-// 16 - 02
-const person = {
-    name: 'Lee'
+//예제 17 - 01
+// 빈 객체의 생성
+const person = new Object();
+
+// 프로퍼티 추가
+person.name = 'Lee';
+person.sayHello = function () {
+    console.log('Hi! My name is ' + this.name);
 };
 
-// 프로퍼티 어트리뷰트 정보를 제공하는 프로퍼티 디스크립터 객체를 반환한다.
-console.log(Object.getOwnPropertyDescriptor(person, 'name'));
-// {value: "Lee", writable: true, enumerable: true, configurable: true}
-// 16 - 03
-const person = {
-    name: 'Lee'
-};
+console.log(person); // {name: "Lee", sayHello: ƒ}
+person.sayHello(); // Hi! My name is Lee
+//예제 17 - 02
+// String 생성자 함수에 의한 String 객체 생성
+const strObj = new String('Lee');
+console.log(typeof strObj); // object
+console.log(strObj); // String {"Lee"}
 
-// 프로퍼티 동적 생성
-person.age = 20;
+// Number 생성자 함수에 의한 Number 객체 생성
+const numObj = new Number(123);
+console.log(typeof numObj); // object
+console.log(numObj); // Number {123}
 
-// 모든 프로퍼티의 프로퍼티 어트리뷰트 정보를 제공하는 프로퍼티 디스크립터 객체들을 반환한다.
-console.log(Object.getOwnPropertyDescriptors(person));
-/*
-{
-  name: {value: "Lee", writable: true, enumerable: true, configurable: true},
-  age: {value: 20, writable: true, enumerable: true, configurable: true}
-}
-*/
-// 16 - 04
-const person = {
-    name: 'Lee'
-};
+// Boolean 생성자 함수에 의한 Boolean 객체 생성
+const boolObj = new Boolean(true);
+console.log(typeof boolObj); // object
+console.log(boolObj); // Boolean {true}
 
-// 프로퍼티 어트리뷰트 정보를 제공하는 프로퍼티 디스크립터 객체를 취득한다.
-console.log(Object.getOwnPropertyDescriptor(person, 'name'));
-// {value: "Lee", writable: true, enumerable: true, configurable: true}
-// 16 - 05
-const person = {
-    name: 'Lee'
-};
+// Function 생성자 함수에 의한 Function 객체(함수) 생성
+const func = new Function('x', 'return x * x');
+console.log(typeof func); // function
+console.dir(func); // ƒ anonymous(x)
 
-// 프로퍼티 동적 생성
-person.age = 20;
+// Array 생성자 함수에 의한 Array 객체(배열) 생성
+const arr = new Array(1, 2, 3);
+console.log(typeof arr); // object
+console.log(arr); // [1, 2, 3]
 
-console.log(Object.getOwnPropertyDescriptors(person));
-/*
-{
-  name: {value: "Lee", writable: true, enumerable: true, configurable: true},
-  age: {value: 20, writable: true, enumerable: true, configurable: true}
-}
-*/
-// 16 - 06
-const person = {
-    // 데이터 프로퍼티
-    firstName: 'Ungmo',
-    lastName: 'Lee',
+// RegExp 생성자 함수에 의한 RegExp 객체(정규 표현식) 생성
+const regExp = new RegExp(/ab+c/i);
+console.log(typeof regExp); // object
+console.log(regExp); // /ab+c/i
 
-    // fullName은 접근자 함수로 구성된 접근자 프로퍼티다. getter 함수
-    get fullName() {
-        return `${this.firstName} ${this.lastName}`;
-    },
-    // setter 함수
-    set fullName(name) {
-        // 배열 디스트럭처링 할당: "31.1 배열 디스트럭처링 할당" 참고
-        [this.firstName, this.lastName] = name.split(' ');
+// Date 생성자 함수에 의한 Date 객체 생성
+const date = new Date();
+console.log(typeof date); // object
+console.log(date); // Mon May 04 2020 08:36:33 GMT+0900 (대한민국 표준시)
+//예제 17 - 03
+const circle1 = {
+    radius: 5,
+    getDiameter() {
+        return 2 * this.radius;
     }
 };
 
-// 데이터 프로퍼티를 통한 프로퍼티 값의 참조.
-console.log(person.firstName + ' ' + person.lastName); // Ungmo Lee
+console.log(circle1.getDiameter()); // 10
 
-// 접근자 프로퍼티를 통한 프로퍼티 값의 저장 접근자 프로퍼티 fullName에 값을 저장하면 setter 함수가 호출된다.
-person.fullName = 'Heegun Lee';
-console.log(person); // {firstName: "Heegun", lastName: "Lee"}
-
-// 접근자 프로퍼티를 통한 프로퍼티 값의 참조 접근자 프로퍼티 fullName에 접근하면 getter 함수가 호출된다.
-console.log(person.fullName); // Heegun Lee
-
-// firstName은 데이터 프로퍼티다. 데이터 프로퍼티는 [[Value]], [[Writable]], [[Enumerable]],
-// [[Configurable]] 프로퍼티 어트리뷰트를 갖는다.
-let descriptor = Object.getOwnPropertyDescriptor(person, 'firstName');
-console.log(descriptor);
-// {value: "Heegun", writable: true, enumerable: true, configurable: true}
-// fullName은 접근자 프로퍼티다. 접근자 프로퍼티는 [[Get]], [[Set]], [[Enumerable]],
-// [[Configurable]] 프로퍼티 어트리뷰트를 갖는다.
-descriptor = Object.getOwnPropertyDescriptor(person, 'fullName');
-console.log(descriptor);
-// {get: ƒ, set: ƒ, enumerable: true, configurable: true}
-// 16 - 08
-const person = {};
-
-// 데이터 프로퍼티 정의
-Object.defineProperty(person, 'firstName', {
-    value: 'Ungmo',
-    writable: true,
-    enumerable: true,
-    configurable: true
-});
-
-Object.defineProperty(person, 'lastName', {value: 'Lee'});
-
-let descriptor = Object.getOwnPropertyDescriptor(person, 'firstName');
-console.log('firstName', descriptor);
-// firstName {value: "Ungmo", writable: true, enumerable: true, configurable:
-// true} 디스크립터 객체의 프로퍼티를 누락시키면 undefined, false가 기본값이다.
-descriptor = Object.getOwnPropertyDescriptor(person, 'lastName');
-console.log('lastName', descriptor);
-// lastName {value: "Lee", writable: false, enumerable: false, configurable:
-// false} [[Enumerable]]의 값이 false인 경우 해당 프로퍼티는 for...in 문이나 Object.keys 등으로 열거할
-// 수 없다. lastName 프로퍼티는 [[Enumerable]]의 값이 false이므로 열거되지 않는다.
-console.log(Object.keys(person)); // ["firstName"]
-
-// [[Writable]]의 값이 false인 경우 해당 프로퍼티의 [[Value]]의 값을 변경할 수 없다. lastName 프로퍼티는
-// [[Writable]]의 값이 false이므로 값을 변경할 수 없다. 이때 값을 변경하면 에러는 발생하지 않고 무시된다.
-person.lastName = 'Kim';
-
-// [[Configurable]]의 값이 false인 경우 해당 프로퍼티를 삭제할 수 없다. lastName 프로퍼티는
-// [[Configurable]]의 값이 false이므로 삭제할 수 없다. 이때 프로퍼티를 삭제하면 에러는 발생하지 않고 무시된다.
-delete person.lastName;
-
-// [[Configurable]]의 값이 false인 경우 해당 프로퍼티를 재정의할 수 없다.
-// Object.defineProperty(person, 'lastName', { enumerable: true }); Uncaught
-// TypeError: Cannot redefine property: lastName
-
-descriptor = Object.getOwnPropertyDescriptor(person, 'lastName');
-console.log('lastName', descriptor);
-// lastName {value: "Lee", writable: false, enumerable: false, configurable:
-// false} 접근자 프로퍼티 정의
-Object.defineProperty(person, 'fullName', {
-    // getter 함수
-    get() {
-        return `${this.firstName} ${this.lastName}`;
-    },
-    // setter 함수
-    set(name) {
-        [this.firstName, this.lastName] = name.split(' ');
-    },
-    enumerable: true,
-    configurable: true
-});
-
-descriptor = Object.getOwnPropertyDescriptor(person, 'fullName');
-console.log('fullName', descriptor);
-// fullName {get: ƒ, set: ƒ, enumerable: true, configurable: true}
-
-person.fullName = 'Heegun Lee';
-console.log(person); // {firstName: "Heegun", lastName: "Lee"}
-// 16 - 09
-const person = {};
-
-Object.defineProperties(person, {
-    // 데이터 프로퍼티 정의
-    firstName: {
-        value: 'Ungmo',
-        writable: true,
-        enumerable: true,
-        configurable: true
-    },
-    lastName: {
-        value: 'Lee',
-        writable: true,
-        enumerable: true,
-        configurable: true
-    },
-    // 접근자 프로퍼티 정의
-    fullName: {
-        // getter 함수
-        get() {
-            return `${this.firstName} ${this.lastName}`;
-        },
-        // setter 함수
-        set(name) {
-            [this.firstName, this.lastName] = name.split(' ');
-        },
-        enumerable: true,
-        configurable: true
-    }
-});
-
-person.fullName = 'Heegun Lee';
-console.log(person); // {firstName: "Heegun", lastName: "Lee"}
-// 16 - 10
-const person = {
-    name: 'Lee'
-};
-
-// person 객체는 확장이 금지된 객체가 아니다.
-console.log(Object.isExtensible(person)); // true
-
-// person 객체의 확장을 금지하여 프로퍼티 추가를 금지한다.
-Object.preventExtensions(person);
-
-// person 객체는 확장이 금지된 객체다.
-console.log(Object.isExtensible(person)); // false
-
-// 프로퍼티 추가가 금지된다.
-person.age = 20; // 무시. strict mode에서는 에러
-console.log(person); // {name: "Lee"}
-
-// 프로퍼티 추가는 금지되지만 삭제는 가능하다.
-delete person.name;
-console.log(person); // {}
-
-// 프로퍼티 정의에 의한 프로퍼티 추가도 금지된다.
-Object.defineProperty(person, 'age', {value: 20});
-// TypeError: Cannot define property age, object is not extensible
-// 16 - 11
-const person = {
-    name: 'Lee'
-};
-
-// person 객체는 밀봉(seal)된 객체가 아니다.
-console.log(Object.isSealed(person)); // false
-
-// person 객체를 밀봉(seal)하여 프로퍼티 추가, 삭제, 재정의를 금지한다.
-Object.seal(person);
-
-// person 객체는 밀봉(seal)된 객체다.
-console.log(Object.isSealed(person)); // true
-
-// 밀봉(seal)된 객체는 configurable이 false다.
-console.log(Object.getOwnPropertyDescriptors(person));
-/*
-{
-  name: {value: "Lee", writable: true, enumerable: true, configurable: false},
-}
-*/
-
-// 프로퍼티 추가가 금지된다.
-person.age = 20; // 무시. strict mode에서는 에러
-console.log(person); // {name: "Lee"}
-
-// 프로퍼티 삭제가 금지된다.
-delete person.name; // 무시. strict mode에서는 에러
-console.log(person); // {name: "Lee"}
-
-// 프로퍼티 값 갱신은 가능하다.
-person.name = 'Kim';
-console.log(person); // {name: "Kim"}
-
-// 프로퍼티 어트리뷰트 재정의가 금지된다.
-Object.defineProperty(person, 'name', {configurable: true});
-// TypeError: Cannot redefine property: name
-// 16 - 12
-const person = {
-    name: 'Lee'
-};
-
-// person 객체는 동결(freeze)된 객체가 아니다.
-console.log(Object.isFrozen(person)); // false
-
-// person 객체를 동결(freeze)하여 프로퍼티 추가, 삭제, 재정의, 쓰기를 금지한다.
-Object.freeze(person);
-
-// person 객체는 동결(freeze)된 객체다.
-console.log(Object.isFrozen(person)); // true
-
-// 동결(freeze)된 객체는 writable과 configurable이 false다.
-console.log(Object.getOwnPropertyDescriptors(person));
-/*
-{
-  name: {value: "Lee", writable: false, enumerable: true, configurable: false},
-}
-*/
-
-// 프로퍼티 추가가 금지된다.
-person.age = 20; // 무시. strict mode에서는 에러
-console.log(person); // {name: "Lee"}
-
-// 프로퍼티 삭제가 금지된다.
-delete person.name; // 무시. strict mode에서는 에러
-console.log(person); // {name: "Lee"}
-
-// 프로퍼티 값 갱신이 금지된다.
-person.name = 'Kim'; // 무시. strict mode에서는 에러
-console.log(person); // {name: "Lee"}
-
-// 프로퍼티 어트리뷰트 재정의가 금지된다.
-Object.defineProperty(person, 'name', {configurable: true});
-// TypeError: Cannot redefine property: name
-// 16 - 13
-const person = {
-    name: 'Lee',
-    address: {
-        city: 'Seoul'
+const circle2 = {
+    radius: 10,
+    getDiameter() {
+        return 2 * this.radius;
     }
 };
 
-// 얕은 객체 동결
-Object.freeze(person);
-
-// 직속 프로퍼티만 동결한다.
-console.log(Object.isFrozen(person)); // true
-// 중첩 객체까지 동결하지 못한다.
-console.log(Object.isFrozen(person.address)); // false
-
-person.address.city = 'Busan';
-console.log(person); // {name: "Lee", address: {city: "Busan"}}
-
-// 16 - 14
-function deepFreeze(target) {
-    // 객체가 아니거나 동결된 객체는 무시하고 객체이고 동결되지 않은 객체만 동결한다.
-    if (target && typeof target === 'object' && !Object.isFrozen(target)) {
-        Object.freeze(target);
-        /*
-      모든 프로퍼티를 순회하며 재귀적으로 동결한다.
-      Object.keys 메서드는 객체 자신의 열거 가능한 프로퍼티 키를 배열로 반환한다.
-      ("19.15.2. Object.keys/values/entries 메서드" 참고)
-      forEach 메서드는 배열을 순회하며 배열의 각 요소에 대하여 콜백 함수를 실행한다.
-      ("27.9.2. Array.prototype.forEach" 참고)
-    */
-        Object
-            .keys(target)
-            .forEach(key => deepFreeze(target[key]));
-    }
-    return target;
+console.log(circle2.getDiameter()); // 20
+//예제 17 - 04
+// 생성자 함수
+function Circle(radius) {
+    // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
 }
 
-const person = {
-    name: 'Lee',
-    address: {
-        city: 'Seoul'
-    }
+// 인스턴스의 생성
+const circle1 = new Circle(5); // 반지름이 5인 Circle 객체를 생성
+const circle2 = new Circle(10); // 반지름이 10인 Circle 객체를 생성
+
+console.log(circle1.getDiameter()); // 10
+console.log(circle2.getDiameter()); // 20
+//예제 17 - 05
+// 함수는 다양한 방식으로 호출될 수 있다.
+function foo() {
+    console.log(this);
+}
+
+// 일반적인 함수로서 호출 전역 객체는 브라우저 환경에서는 window, Node.js 환경에서는 global을 가리킨다.
+foo(); // window
+
+// 메서드로서 호출
+const obj = {
+    foo
+}; // ES6 프로퍼티 축약 표현
+obj.foo(); // obj
+
+// 생성자 함수로서 호출
+const inst = new foo(); // inst
+//예제 17 - 06
+// new 연산자와 함께 호출하지 않으면 생성자 함수로 동작하지 않는다. 즉, 일반 함수로서 호출된다.
+const circle3 = Circle(15);
+
+// 일반 함수로서 호출된 Circle은 반환문이 없으므로 암묵적으로 undefined를 반환한다.
+console.log(circle3); // undefined
+
+// 일반 함수로서 호출된 Circle내의 this는 전역 객체를 가리킨다.
+console.log(radius); // 15
+
+//예제 17 - 08
+function Circle(radius) {
+    // 1. 암묵적으로 빈 객체가 생성되고 this에 바인딩된다.
+    console.log(this); // Circle {}
+
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+}
+//예제 17 - 09
+function Circle(radius) {
+    // 1. 암묵적으로 인스턴스가 생성되고 this에 바인딩된다.
+
+    // 2. this에 바인딩되어 있는 인스턴스를 초기화한다.
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+}
+//예제 17 - 10
+function Circle(radius) {
+    // 1. 암묵적으로 인스턴스가 생성되고 this에 바인딩된다.
+
+    // 2. this에 바인딩되어 있는 인스턴스를 초기화한다.
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+
+    // 3. 완성된 인스턴스가 바인딩된 this가 암묵적으로 반환된다
+}
+
+// 인스턴스 생성. Circle 생성자 함수는 암묵적으로 this를 반환한다.
+const circle = new Circle(1);
+console.log(circle); // Circle {radius: 1, getDiameter: ƒ}
+//예제 17 - 11
+function Circle(radius) {
+    // 1. 암묵적으로 인스턴스가 생성되고 this에 바인딩된다.
+
+    // 2. this에 바인딩되어 있는 인스턴스를 초기화한다.
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+
+    // 3. 암묵적으로 this를 반환한다. 명시적으로 객체를 반환하면 암묵적인 this 반환이 무시된다.
+    return {};
+}
+
+// 인스턴스 생성. Circle 생성자 함수는 명시적으로 반환한 객체를 반환한다.
+const circle = new Circle(1);
+console.log(circle); // {}
+//예제 17 - 12
+function Circle(radius) {
+    // 1. 암묵적으로 인스턴스가 생성되고 this에 바인딩된다.
+
+    // 2. this에 바인딩되어 있는 인스턴스를 초기화한다.
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+
+    // 3. 암묵적으로 this를 반환한다. 명시적으로 원시값을 반환하면 원시값 반환은 무시되고 암묵적으로 this가 반환된다.
+    return 100;
+}
+
+// 인스턴스 생성. Circle 생성자 함수는 명시적으로 반환한 객체를 반환한다.
+const circle = new Circle(1);
+console.log(circle); // Circle {radius: 1, getDiameter: ƒ}
+//예제 17 - 13
+// 함수는 객체다.
+function foo() {}
+
+// 함수는 객체이므로 프로퍼티를 소유할 수 있다.
+foo.prop = 10;
+
+// 함수는 객체이므로 메서드를 소유할 수 있다.
+foo.method = function () {
+    console.log(this.prop);
 };
 
-// 깊은 객체 동결
-deepFreeze(person);
+foo.method(); // 10
+//예제 17 - 14
+function foo() {}
 
-console.log(Object.isFrozen(person)); // true
-// 중첩 객체까지 동결한다.
-console.log(Object.isFrozen(person.address)); // true
+// 일반적인 함수로서 호출: [[Call]]이 호출된다.
+foo();
 
-person.address.city = 'Busan';
-console.log(person); // {name: "Lee", address: {city: "Seoul"}}
+// 생성자 함수로서 호출: [[Construct]]가 호출된다.
+new foo();
+//예제 17 - 15
+// 일반 함수 정의: 함수 선언문, 함수 표현식
+function foo() {}
+const bar = function () {};
+// 프로퍼티 x의 값으로 할당된 것은 일반 함수로 정의된 함수다. 이는 메서드로 인정하지 않는다.
+const baz = {
+    x: function () {}
+};
+
+// 일반 함수로 정의된 함수만이 constructor이다.
+new foo(); // -> foo {}
+new bar(); // -> bar {}
+new baz.x(); // -> x {}
+
+// 화살표 함수 정의
+const arrow = () => {};
+
+new arrow(); // TypeError: arrow is not a constructor
+
+// 메서드 정의: ES6의 메서드 축약 표현만을 메서드로 인정한다.
+const obj = {
+    x() {}
+};
+
+new obj.x(); // TypeError: obj.x is not a constructor
+//예제 17 - 16
+function foo() {}
+
+// 일반 함수로서 호출 [[Call]]이 호출된다. 모든 함수 객체는 [[Call]]이 구현되어 있다.
+foo();
+
+// 생성자 함수로서 호출 [[Construct]]가 호출된다. 이때 [[Construct]]를 갖지 않는다면 에러가 발생한다.
+new foo();
+//예제 17 - 17
+// 생성자 함수로서 정의하지 않은 일반 함수
+function add(x, y) {
+    return x + y;
+}
+
+// 생성자 함수로서 정의하지 않은 일반 함수를 new 연산자와 함께 호출
+let inst = new add();
+// 함수가 객체를 반환하지 않았으므로 반환문이 무시된다. 따라서 빈 객체가 생성되어 반환된다.
+console.log(inst); // {}
+
+// 객체를 반환하는 일반 함수
+function createUser(name, role) {
+    return {name, role};
+}
+
+// 생성자 함수로서 정의하지 않은 일반 함수를 new 연산자와 함께 호출
+inst = new createUser('Lee', 'admin');
+// 함수가 생성한 객체를 반환한다.
+console.log(inst); // {name: "Lee", role: "admin"}
+//예제 17 - 18
+// 생성자 함수
+function Circle(radius) {
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+}
+
+// new 연산자 없이 생성자 함수 호출하면 일반 함수로서 호출된다.
+const circle = Circle(5);
+console.log(circle); // undefined
+
+// 일반 함수 내부의 this는 전역 객체 window를 가리킨다.
+console.log(radius); // 5
+console.log(getDiameter()); // 10
+
+circle.getDiameter();
+// TypeError: Cannot read property 'getDiameter' of undefined
+//예제 17 - 19
+// 생성자 함수
+function Circle(radius) {
+    // 이 함수가 new 연산자와 함께 호출되지 않았다면 new.target은 undefined다.
+    if (!new.target) {
+        // new 연산자와 함께 생성자 함수를 재귀 호출하여 생성된 인스턴스를 반환한다.
+        return new Circle(radius);
+    }
+
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+}
+
+// new 연산자 없이 생성자 함수를 호출하여도 new.target을 통해 생성자 함수로서 호출된다.
+const circle = Circle(5);
+console.log(circle.getDiameter());
+//예제 17 - 20
+// Scope-Safe Constructor Pattern
+function Circle(radius) {
+    // 생성자 함수가 new 연산자와 함께 호출되면 함수의 선두에서 빈 객체를 생성하고 this에 바인딩한다. 이때 this와 Circle은
+    // 프로토타입에 의해 연결된다. 이 함수가 new 연산자와 함께 호출되지 않았다면 이 시점의 this는 전역 객체 window를 가리킨다.
+    // 즉, this와 Circle은 프로토타입에 의해 연결되지 않는다.
+    if (!(this instanceof Circle)) {
+        // new 연산자와 함께 호출하여 생성된 인스턴스를 반환한다.
+        return new Circle(radius);
+    }
+
+    this.radius = radius;
+    this.getDiameter = function () {
+        return 2 * this.radius;
+    };
+}
+
+// new 연산자 없이 생성자 함수를 호출하여도 생성자 함수로서 호출된다.
+const circle = Circle(5);
+console.log(circle.getDiameter()); // 10

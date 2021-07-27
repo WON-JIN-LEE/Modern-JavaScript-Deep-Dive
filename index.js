f459549c368ffe21d02c48c36764852262c1db29
WON-JIN-LEE <vopgh0409@gmail.com>
2021-07-27 16:08:10 +0900

    Add JS-CH.45

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-let grandeur = { engine: "gdi", cc: 3000 };
-let aa = { name: "kildong", age: 20, car: grandeur };
-let bb = JSON.parse(JSON.stringify(aa)); // Object Deep Copy, 하위Object Deep Copy
+const promiseGet = url => {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
 
-console.log(Object.is(aa, bb)); // false
-console.log(Object.is(aa.car, bb.car)); // false
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        // 성공적으로 응답을 전달받으면 resolve 함수를 호출한다.
+        resolve(JSON.parse(xhr.response));
+      } else {
+        // 에러 처리를 위해 reject 함수를 호출한다.
+        reject(new Error(xhr.status));
+      }
+    };
+  });
+};
+
+// promiseGet 함수는 프로미스를 반환한다.
+promiseGet('https://jsonplaceholder.typicode.com/posts/1')
+  .then(res => console.log(res))
+  .catch(err => console.error(err))
+  .finally(() => console.log('Bye!'));
